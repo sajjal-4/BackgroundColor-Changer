6ada4a30ceed7c282ab2d82159b062787909df56
Sajjal Khalil <sajjalkhalil4@gmail.com>
2025-02-15 14:21:15 +0500

    Add files via upload

---
+++ BackgroundColor/Script.js
@@ -0,0 +1,15 @@
+document.getElementById('colorButton').addEventListener('click',myfunction)
+function myfunction() 
+{
+    document.body.style.backgroundColor = getRandomColor();
+}
+function getRandomColor() 
+{
+    const letters='0123456789ABCDEF';
+    let color='#';
+    for(let i=0;i<6;i++) 
+    {
+        color+=letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
